77bc48dfc8c4168999641a9729b9bdc54b28449b
peterhoejlundandersen <hilsenit@gmail.com>
2017-05-30 11:17:48 +0200

    changed Amazon keys in Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"], 
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], 
     region: 'eu-west-2'       # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV["S3_BUCKET_NAME"]              # required
 end
