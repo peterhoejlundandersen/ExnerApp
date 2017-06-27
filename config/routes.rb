db8f3544974d33a18a83a0c5b200aafaaeb49863
peterhoejlundandersen <hilsenit@gmail.com>
2017-06-27 10:31:47 +0200

    trying last time with pdfjs gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :johannes_writings do
-  end
+
+
 
 	devise_for :users, path: '', path_names: { sign_in: 'log-in', sign_up: 'opret-bruger' } 
 	
@@ -13,11 +13,12 @@ Rails.application.routes.draw do
 			put :sort_images
 		end
 	end
-		
+	
+	mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
+	resources :johannes_writings do
+	end
+
 	get 'om-inger-og-johannes-exner' => 'static_pages#about', as: "om"
 
-	get 'billede-uploader' => 'image_uploaders#uploader', as: "image_uploader"
-	post 'billede-uploader' => 'image_uploaders#create'
-	
 	root 'static_pages#frontpage'
 end
