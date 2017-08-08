ad8f9fef0b09e1df5296554a4705e4c649f3c5df
peterhoejlundandersen <hilsenit@gmail.com>
2017-08-08 14:10:58 +0200

    trying without 'alle' in pdf-categories category

--- app/controllers/pdf_categories_controller.rb
+++ app/controllers/pdf_categories_controller.rb
@@ -2,13 +2,10 @@ class PdfCategoriesController < ApplicationController
 	layout "works"
 
 	def index
-		unless params[:category_id] == "0"
 			@pdf_categories = PdfCategory.all
 			@pdfs = PdfCategory.friendly.find(params[:category_id]).pdfs
-		else 
-			@pdf_categories = PdfCategory.all
-			@pdfs = Pdf.all
-		end
+			# @pdf_categories = PdfCategory.all
+			# @pdfs = Pdf.all
 	end
 
 end
