b339ad16e1da242e8837ac1de7206fcd3b4103bf
peterhoejlundandersen <hilsenit@gmail.com>
2017-08-08 15:19:35 +0200

    trying to find out what it is

--- app/controllers/pdf_categories_controller.rb
+++ app/controllers/pdf_categories_controller.rb
@@ -2,8 +2,9 @@ class PdfCategoriesController < ApplicationController
 	layout "works"
 
 	def index
-			@pdf_categories = PdfCategory.all
-			@pdfs = PdfCategory.friendly.find(params[:category_id]).pdfs
+		render 'hallo'
+			# @pdf_categories = PdfCategory.all
+			# @pdfs = PdfCategory.friendly.find(params[:category_id]).pdfs
 			# @pdf_categories = PdfCategory.all
 			# @pdfs = Pdf.all
 	end
