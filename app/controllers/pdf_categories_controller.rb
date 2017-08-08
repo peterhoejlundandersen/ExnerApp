b98a0912454f448b91dbf1324c94bd95d67a9efb
peterhoejlundandersen <hilsenit@gmail.com>
2017-08-08 15:31:15 +0200

    trying simple approach again

--- app/controllers/pdf_categories_controller.rb
+++ app/controllers/pdf_categories_controller.rb
@@ -2,8 +2,8 @@ class PdfCategoriesController < ApplicationController
 	layout "works"
 
 	def index
+		@pdf_categories = PdfCategory.all
 		render 'hallo'
-			@pdf_categories = PdfCategory.all
 			# @pdfs = PdfCategory.friendly.find(params[:category_id]).pdfs
 			# @pdf_categories = PdfCategory.all
 			# @pdfs = Pdf.all
