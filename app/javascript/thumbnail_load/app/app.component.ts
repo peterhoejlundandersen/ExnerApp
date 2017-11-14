63bf7d1a18222ddd2a11e479cab4c131d2cd1d66
peterhoejlundandersen <hilsenit@gmail.com>
2017-11-14 14:33:26 +0100

    thumbnail call is working with a seperate image service, injectionk. BUwaaah

--- app/javascript/thumbnail_load/app/app.component.ts
+++ app/javascript/thumbnail_load/app/app.component.ts
@@ -2,36 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
 import { Http, HttpModule } from '@angular/http';
 
+import { ImageService } from './image.service';
 
 @Component({
   selector: 'thumbnail-load',
   template: `
   <div class="thumb-images row sortable-images">
-    <div *ngFor="let image of images" 
+    <div *ngFor="let thumb_image of thumb_images" 
     class="text-center thumb-image col-lg-2 col-md-3 col-6 col-xs-6 sortable-image-item"
-    [attr.data-id]="image.id" data-type="Image">
-      <img src="{{image.thumb_url}}">
+    [attr.data-id]="thumb_image.id" data-type="Image">
+      <img src="{{thumb_image.thumb_url}}">
     </div>
   </div>
   `,
 })
 export class AppComponent implements OnInit {
-  images: null;
+  thumb_images: null;
   constructor( 
-    public http: Http
+    public http: Http,
+    private image_service: ImageService
   ) {};
-
-
-  getThumbImages(image_category_id) {
-    this.http.get('/works/images.json?image_category_id=' + image_category_id)
-      .subscribe(
-        data => this.images = data.json().images,
-        err => console.log(err)
-      )
-  };
 
   ngOnInit() {
     var img_cat_id = document.getElementById('imgCat').getAttribute("data-img-cat");
-    this.getThumbImages(img_cat_id);
+    this.image_service.getThumbnailImages(img_cat_id)
+      .subscribe(data => this.thumb_images = data.images);
   }
 }
--- app/javascript/thumbnail_load/app/app.module.ts
+++ app/javascript/thumbnail_load/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 
+import { ImageService } from './image.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
