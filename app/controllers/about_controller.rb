3be6e1b682c45c21f507a42a586a9e782218fa5a
peterhoejlundandersen <hilsenit@gmail.com>
2017-11-21 17:31:21 +0100

    insetted videos and set them up

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,2 +1,7 @@
 class AboutController < ApplicationController
+  layout 'works'
+  def index
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,45 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'log-in', sign_up: 'opret-bruger' }
 
+  resources :categories, except: :index
+  get '/oversigt' => 'categories#index', as: "oversigt"
 
-	get 'pdfs/show'
+  resources :works, except: :index do
+    put :sort, on: :collection
+    member do
+      put :overview_img
+      put :sort_images
+      get :new_image
+      get :new_image_category
+    end
+  end
+  get 'vaerker/:vaerker_cat' => 'works#index', as: :vaerker
 
-	get 'pdfs/edit'
+  get 'vaerker/design/:vaerker_cat' => 'works#design_index', as: :vaerker_design
 
-	get 'pdfs/new'
+  ### PDFS ###
 
-	devise_for :users, path: '', path_names: { sign_in: 'log-in', sign_up: 'opret-bruger' } 
+  get 'pdfs/show'
+  get 'pdfs/edit'
+  get 'pdfs/new'
 
-	resources :categories, except: :index
-	get '/oversigt' => 'categories#index', as: "oversigt"
+  resources :pdf_categories, except: [:index] do
+    resources :pdfs, except: [:index]
+  end
 
-	resources :works, except: :index do
-		put :sort, on: :collection
-		member do
-			put :overview_img
-			put :sort_images
-			get :new_image
-			get :new_image_category
-		end
-	end
-	get 'vaerker/:vaerker_cat' => 'works#index', as: :vaerker 
+  get "johannes-exners-tekster/vis-alle" => "pdf_categories#index", as: "show_pdf_category"
+  get "johannes-exners-tekster/:kategori_id" => "pdf_categories#show_category", as: "show_this_pdf_category"
+  get 'pdf-views/show' => 'pdf_views#show', as: 'pdf_view'
 
-	get 'vaerker/design/:vaerker_cat' => 'works#design_index', as: :vaerker_design	
+  ### PDFS END ###
 
-	### PDFS ### 	
+  get 'om-inger-og-johannes-exner' => 'about#index', as: "om"
 
-	resources :pdf_categories, except: [:index] do 
-		resources :pdfs, except: [:index] 
-	end 
-
-	get "johannes-exners-tekster/vis-alle" => "pdf_categories#index", as: "show_pdf_category"
-
-	get "johannes-exners-tekster/:kategori_id" => "pdf_categories#show_category", as: "show_this_pdf_category" 
-
-	get 'pdf-views/show' => 'pdf_views#show', as: 'pdf_view'
-
-	### PDFS END ### 
-
-	get 'om-inger-og-johannes-exner' => 'static_pages#about', as: "om"
-
-	#FORSIDE TEKST
-	get 'forside/ny-forside-tekst' => 'static_pages#new_frontpage_text', as: :ny_forside_tekst
-	get 'forside/rediger-forside-tekst/:text_id' => 'static_pages#edit_frontpage_text', as: :rediger_forside_tekst
-	post 'forside/opret-forside-tekst' => 'static_pages#create_frontpage_text', as: :opret_forside_tekst
-	patch 'forside/opdater-tekst/:id' => 'static_pages#update_frontpage_text', as: :opdater_forside_tekst 
-	# END FORSIDE TEKST
-	root 'static_pages#frontpage'
+  #FORSIDE TEKST
+  get 'forside/ny-forside-tekst' => 'static_pages#new_frontpage_text', as: :ny_forside_tekst
+  get 'forside/rediger-forside-tekst/:text_id' => 'static_pages#edit_frontpage_text', as: :rediger_forside_tekst
+  post 'forside/opret-forside-tekst' => 'static_pages#create_frontpage_text', as: :opret_forside_tekst
+  patch 'forside/opdater-tekst/:id' => 'static_pages#update_frontpage_text', as: :opdater_forside_tekst
+  # END FORSIDE TEKST
+  root 'static_pages#frontpage'
 end
