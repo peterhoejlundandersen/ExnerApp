44fef8c7eb4ae9ae6ba696196e67fb61fe768320
peterhoejlundandersen <hilsenit@gmail.com>
2018-01-05 13:33:21 +0100

    video is shown on video index, and validation

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,9 +4,26 @@ class VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:notice] = "#{@video.title} er blevet oprettet."
+      redirect_to videos_path()
+    else
+      flash.now[:notice] = "Noget gik galt, prøv igen. Er titlen skrevet ind?"
+      render :new
+    end
+  end
+
   def edit
   end
 
   def index
+    @videos = Video.all
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:title, :description, :only_link, :video_url, :link)
   end
 end
