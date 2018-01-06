9d38ec0b0f4950d1d52edef739ac085ad6931839
peterhoejlundandersen <hilsenit@gmail.com>
2018-01-06 13:41:50 +0100

    added timeline and created it so it loads after rendering

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,11 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( works.scss frontpage.scss work_images.js works.js html.sortable.js pdfs font_calls)
+Rails.application.config.assets.precompile += %w( works.scss frontpage.scss work_images.js works.js html.sortable.js pdfs font_calls about.scss)
 
+Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path| # Subdirectories are added to images assets path
+  Rails.application.config.assets.paths << path
+end
 ## PDF ##
 
 # application.js, application.css, and all non-JS/CSS in the app/assets
