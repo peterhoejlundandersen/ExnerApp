6736ddfceeb002d16b7fcd14d280fbfac9b73079
peterhoejlundandersen <hilsenit@gmail.com>
2018-01-11 22:18:35 +0100

    drag and drop with dragular is returning index and id in an array of object

--- app/javascript/works_index/app/works_index.component.ts
+++ app/javascript/works_index/app/works_index.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit  } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from "@angular/platform-browser";
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
 import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'works-index',
   template: `
-  <div class="row sortable works-wrapper" [dragula]='"first-bag"'> 
-    <div *ngFor="let work of works; let i = index" [class.vertical-image]="organs" class="overview-work sortable-item" [attr.data-id]="work.id" [attr.data-type]="">
+  <div class="row sortable works-wrapper" [dragula]='"first-bag"' [dragulaModel]='works'> 
+    <div *ngFor="let work of works; let i = index" [attr.data-drag-i]="i" [class.vertical-image]="organs" class="overview-work sortable-item" [attr.data-id]="work.id" [attr.data-type]="">
           <img *ngIf="work.overview_img.url" [src]="work.overview_img.url" (load)="assignImageValue(i, work.overview_img.url)" hidden>
           <a [href]="'/works/' + work.slug">
             <div *ngIf="!work.overview_img.url" class="overview-img-block overview-img-block-show work-without-image">
@@ -36,7 +37,25 @@ import 'rxjs/add/operator/map';
 export class WorksIndexComponent implements OnInit {
   organs: boolean = false;
   works: {}[] = [];
-  constructor( private http: Http ) {}
+  constructor( 
+    private http: Http,
+    private dragulaService: DragulaService
+  ) {
+  dragulaService.dropModel.subscribe((value) => {
+    var element = value.slice(1)[0];
+    var children = [].slice.call(element.parentElement.children);
+    var sorting_data = new Array();
+    children.forEach(function(image) {
+      sorting_data.push({id: image.dataset.id, order_i: image.dataset.dragI});
+    });
+    console.log(sorting_data);
+    });
+  }
+
+  private getElementIndex(el: any) {
+    return [].slice.call(el.parentElement.children).indexOf(el);
+  }
+
 
   ngOnInit() {
     var category_friendly_id = document.getElementById('categoryId').getAttribute('data-category-id');
