5b157ac820c1f417a6a8a99c993234dbe46c9b7e
peterhoejlundandersen <hilsenit@gmail.com>
2018-01-12 09:17:22 +0100

    added type to dragging items, so i can use it for Images and ImagesCategories as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,18 @@ class ApplicationController < ActionController::Base
   def sorting_objects
     params[:_json].each do |dataset|
       if !dataset[:id].nil? && !dataset[:order_i].nil?
-        params_now = dataset.permit(:order_i, :id)
-        position = params_now[:order_i].to_i
-        work = Work.find(params_now[:id].to_i)
-        work.update_attribute(:position, position)
+        record = case dataset[:type]
+        when "work"
+          Work.find(dataset[:id].to_i)
+        when "image-cat"
+          ImageCategory.find(dataset[:id].to_i)
+        when "image"
+          Image.find(dataset[:id].to_i)
+        else
+          nil
+        end
+        position = dataset[:order_i].to_i
+        record.update_attribute(:position, position) unless record.nil?
       end
     end
   end
--- app/javascript/works_index/app/works_index.component.ts
+++ app/javascript/works_index/app/works_index.component.ts
@@ -9,8 +9,8 @@ import 'rxjs/add/operator/map';
 @Component({
   selector: 'works-index',
   template: `
-  <div class="row sortable works-wrapper" [dragula]='"first-bag"' [dragulaModel]='works'> 
-    <div *ngFor="let work of works; let i = index" [class.vertical-image]="organs" class="overview-work sortable-item" [attr.data-id]="work.id" [attr.data-type]="">
+  <div class="row works-wrapper" [dragula]='"first-bag"' [dragulaModel]='works'> 
+    <div *ngFor="let work of works; let i = index" [class.vertical-image]="organs" class="overview-work" data-type="work" [attr.data-id]="work.id" [attr.data-type]="">
           <img *ngIf="work.overview_img.url" [src]="work.overview_img.url" (load)="assignImageValue(i, work.overview_img.url)" hidden>
           <a [href]="'/works/' + work.slug">
             <div *ngIf="!work.overview_img.url" class="overview-img-block overview-img-block-show work-without-image">
@@ -30,7 +30,6 @@ import 'rxjs/add/operator/map';
     <div [class.vertical-image]="organs" [class.no-drag]="binding"  *ngIf="works.length != 3" class="overview-work hidden-md-down">
       <div class="overview-img-block"></div>
     </div>
-
   </div>
   `
 })
@@ -44,26 +43,18 @@ export class WorksIndexComponent implements OnInit {
   ) {
   dragulaService.dropModel.subscribe((value) => {
     var element = value.slice(1)[0];
-    [].slice.call(element.parentElement.children).indexOf(element)
-    // var children = document.querySelectorAll('overview-work');
-    var sorting_data = new Array();
     var children = [].slice.call(element.parentElement.children);
+    var sorting_data = new Array();
     children.forEach(function(image, index) {
       let index_of_image = children.indexOf(image);
-      debugger;
-      sorting_data.push({id: image.dataset.id, order_i: index_of_image});
+      sorting_data.push({id: image.dataset.id, order_i: index_of_image, type: image.dataset.type});
     });
     var url = "/sorting-objects";
     var body = JSON.stringify(sorting_data);
     let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
     let options = new RequestOptions({ headers: headers }); // Create a request option
     this.http.post(url, body, options).subscribe(
-        data => {
-          console.log("HEEEEY");
-        },
-      err => { 
-        console.log(err);
-      }
+        data => { }, err => { console.log(err); }
       )
     });
   }
