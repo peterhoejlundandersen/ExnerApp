ca50fdf0cc3a720a225044136453a2346b124200
peterhoejlundandersen <hilsenit@gmail.com>
2018-04-22 15:16:01 +0200

    make it with you Angular in rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,18 +3,15 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w( works.scss frontpage.scss work_images.js works.js html.sortable.js pdfs font_calls about.scss, pdfs.scss angular-pdfjs-viewer/bower_components/pdf.js-viewer/viewer.css angular-pdfjs-viewer/bower_components/pdf.js-viewer/pdf.js)
+Rails.application.config.assets.precompile += %w( works.scss frontpage.scss work_images.js works.js html.sortable.js pdfs font_calls about.scss, pdfs.scss)
+
+# PDF JS ASSETS
+Rails.application.config.assets.precompile += %w( angular-pdfjs-viewer/bower_components/pdf.js-viewer/pdf.js angular-pdfjs-viewer/bower_components/pdf.js-viewer/viewer.css angular-pdfjs-viewer/bower_components/angular/angular.js angular-pdfjs-viewer/dist/angular-pdfjs-viewer.js)
 
 Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path| # Subdirectories are added to images assets path
   Rails.application.config.assets.paths << path
 end
-## PDF ##
 
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
