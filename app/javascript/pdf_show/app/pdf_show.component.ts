e28042abb7e53afab3ba25eb22bb41d8a9fd700b
peterhoejlundandersen <hilsenit@gmail.com>
2018-04-22 15:31:14 +0200

    going back to Angular 2 - stop using AngularJS in your project

--- app/javascript/pdf_show/app/pdf_show.component.ts
+++ app/javascript/pdf_show/app/pdf_show.component.ts
@@ -5,10 +5,11 @@ import { pdfjsViewer } from "angular-pdfjs-viewer";
 
 @Component({
   selector: 'pdfjs-viewer',
-  template: `<h1>Heeey</h1>`
+  template: `<pdfjs-viewer src="{{ pdf.src }}></pdfjs-viewer>`
 })
 
 export class PdfShowComponent implements OnInit {
+  pdf: Object
   
   constructor(
   private _http: Http,
@@ -17,6 +18,7 @@ export class PdfShowComponent implements OnInit {
   ngOnInit() {
     var file_path = document.getElementById('pdfId').getAttribute('data-path');
     console.log(file_path);
+    this.pdf = { src: "https://exnerbilleder.s3.eu-west-2.amazonaws.com/pdf/Kirkeforhold/1965.2%20Kirkebygningens%20indre%20og%20ydre%20enhed%20-%20Kirkebygning%20og%20teologi.pdf" }
   }
 }
 
