06fe98bb40eac26eae87573111bad0cf7db634f0
peterhoejlundandersen <hilsenit@gmail.com>
2019-07-30 18:15:32 +0200

    added the field opening year to get an integer instead of the old timestamp for PDFs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,16 +2,19 @@ class SearchController < ApplicationController
   layout "works"
 
   def search
+    return if params[:q].nil?
+
     search_term = params[:q]
 
     is_integer = Integer(search_term) rescue false
 
     #Checking if search is an integer so it searched for year in the datafields
-    sql_pdfs = {int: is_integer, data_field: is_integer ? "date" : "title", data_lookup: is_integer ? "=" : "LIKE"}
-    sql_works = {int: is_integer, data_field: is_integer ? "opening_year" : "name", data_lookup: is_integer ? "=" : "LIKE"}
+    sql_pdfs = {int: is_integer, data_field: is_integer ? "opening_year" : "lower(title)", data_lookup: is_integer ? "=" : "LIKE"}
+    sql_works = {int: is_integer, data_field: is_integer ? "opening_year" : "lower(name)", data_lookup: is_integer ? "=" : "LIKE"}
 
     if search_term.nil? || search_term.empty?
       @works, @articles = []
+      flash_error( "" )
     else
       #Sql search created with the array data_fields
       @works = Work.where( sql_search( sql_works, search_term ), search_word_fuz(search_term, sql_works[:int]) )
@@ -24,7 +27,8 @@ class SearchController < ApplicationController
   private
 
   def sql_search info, search_word
-    "#{info[:data_field]} #{info[:data_lookup]} ?"
+    lower_case_or_not = info[:int] ? "?" : "lower(?)"
+    "#{info[:data_field]} #{info[:data_lookup]} #{lower_case_or_not}"
   end
 
   def search_word_fuz search_word, is_int
@@ -32,7 +36,7 @@ class SearchController < ApplicationController
   end
 
   def flash_error search_word
-    flash[:notice] = "Intet matcher søgningen \"#{search_word}\", desværre"
+    flash.now[:notice] = "Intet matcher søgningen \"#{search_word}\", desværre."
   end
 
 end
