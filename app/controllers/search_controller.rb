c4edfc4ece3febe69a543b312ae035a75b0ab2e1
peterhoejlundandersen <hilsenit@gmail.com>
2019-07-30 16:43:45 +0200

    created a search for both PDFS and WORKS in the controller. Not shown in the view yet

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,20 +2,37 @@ class SearchController < ApplicationController
   layout "works"
 
   def search
+    search_term = params[:q]
 
-    is_integer = Integer(params[:q]) rescue false
+    is_integer = Integer(search_term) rescue false
 
-    if params[:q].nil? || params[:q].empty?
-      @works = []
-    elsif is_integer
-      @works = Work.where('opening_year = ?', "#{params[:q]}")
-      flash[:notice] = "Intet matcher søgningen \"#{params[:q]}\", desværre" unless @works.any?
+    #Checking if search is an integer so it searched for year in the datafields
+    sql_pdfs = {int: is_integer, data_field: is_integer ? "date" : "title", data_lookup: is_integer ? "=" : "LIKE"}
+    sql_works = {int: is_integer, data_field: is_integer ? "opening_year" : "name", data_lookup: is_integer ? "=" : "LIKE"}
+
+    if search_term.nil? || search_term.empty?
+      @works, @articles = []
     else
-      @works = Work.where('name LIKE ?', "%#{params[:q]}%")
-      flash[:notice] = "Intet matcher søgningen \"#{params[:q]}\", desværre" unless @works.any?
+      #Sql search created with the array data_fields
+      @works = Work.where( sql_search( sql_works, search_term ), search_word_fuz(search_term, sql_works[:int]) )
+      @articles = Pdf.where( sql_search( sql_pdfs, search_term ),  search_word_fuz(search_term, sql_pdfs[:int] ) )
+      flash_error( search_term ) unless @works.any? || @articles.any?
     end
+
   end
 
+  private
 
+  def sql_search info, search_word
+    "#{info[:data_field]} #{info[:data_lookup]} ?"
+  end
+
+  def search_word_fuz search_word, is_int
+    is_int ? search_word : "%#{search_word}%"
+  end
+
+  def flash_error search_word
+    flash[:notice] = "Intet matcher søgningen \"#{search_word}\", desværre"
+  end
 
 end
