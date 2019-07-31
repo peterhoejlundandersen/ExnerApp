f00088f83603959e3b2b6fa2e33599dd3f2fcd3a
peterhoejlundandersen <hilsenit@gmail.com>
2019-07-31 11:52:41 +0200

    removed binding.pry from pdf uploader

--- app/uploaders/pdf_image_uploader.rb
+++ app/uploaders/pdf_image_uploader.rb
@@ -12,7 +12,6 @@ class PdfImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    binding.pry
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
